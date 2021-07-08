570d3c963679c07c8d1c4974ab27aec240d9aa14
abhissaw10 <sawhney.abhishek1983@gmail.com>
2021-07-08 20:35:05 +0530

    Added spring security JWT

--- src/main/java/com/bmc/appointmentservice/controller/AppointmentController.java
+++ src/main/java/com/bmc/appointmentservice/controller/AppointmentController.java
@@ -5,6 +5,7 @@ import com.bmc.appointmentservice.model.Prescription;
 import com.bmc.appointmentservice.service.AppointmentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,15 +17,15 @@ public class AppointmentController {
 
     private final AppointmentService appointmentService;
 
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
     @PostMapping("/user/{userId}/appointment")
     public ResponseEntity<String> bookAppointment(@PathVariable String userId, @RequestBody Appointment appointment){
         appointment.setUserId(userId);
         return ResponseEntity.ok(appointmentService.appointment(appointment));
     }
-
+    @PreAuthorize("hasRole('USER')")
     @PostMapping("/user/{userId}/prescription")
     public ResponseEntity<String> prescription(@PathVariable String userId, @RequestBody Prescription prescription){
-
         return ResponseEntity.ok("Success");
     }
 }
--- src/main/java/com/bmc/appointmentservice/controller/DoctorAvailabilityController.java
+++ src/main/java/com/bmc/appointmentservice/controller/DoctorAvailabilityController.java
@@ -5,12 +5,15 @@ import com.bmc.appointmentservice.model.Availability;
 import com.bmc.appointmentservice.service.DoctorAvailabilityService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 public class DoctorAvailabilityController {
     private final DoctorAvailabilityService availabilityService;
+
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
     @PostMapping("/doctor/{id}/availability")
     public ResponseEntity<String> saveDoctorAvailability(@PathVariable String id, @RequestBody Availability availabilityRequest){
         availabilityRequest.setDoctorId(id);
@@ -18,6 +21,7 @@ public class DoctorAvailabilityController {
     return ResponseEntity.ok("Success");
     }
 
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
     @GetMapping("/doctor/{id}/availability")
     public ResponseEntity<Availability> getDoctorAvailability(@PathVariable String id){
         return ResponseEntity.ok(availabilityService.getDoctorAvailability(id));
